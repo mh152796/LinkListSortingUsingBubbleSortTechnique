48ec50270e12b5afbcb5ada7cedf49419102918e
mh152796 <mosaraf15-2796@diu.edu.bd>
2020-09-27 13:38:12 +0600

    my code

---
+++ linked list sorting using bubble sort technique .c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+
+    int data;
+    struct node *next;
+
+};
+struct node *head=NULL;
+void insert(int data)
+{
+    struct node *temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->next=NULL;
+    if(head==NULL)
+    {
+        head=temp;
+        return;
+    }
+    struct node *temp1=head;
+    while(temp1->next!=NULL)
+    {
+        temp1= temp1->next;
+    }
+    temp1->next=temp;
+}
+void bubleSort()
+{
+    struct node *temp1=head,*temp2=head;
+    struct node *p=NULL,*q=NULL,*n=NULL;
+    int swap;
+    while(temp1->next!=NULL)
+    {
+        temp2 = head;
+        while(temp2->next!=NULL)
+        {
+            p = temp2->data;
+            n = temp2 ->next;
+            q =n->data;
+            if(p>q)
+            {
+                temp2->data = q;
+                n->data =p;
+            }
+            temp2= temp2->next;
+        }
+        temp1= temp1->next;
+    }
+}
+void print()
+{
+    struct node *temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+}
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    while(N--)
+    {
+        int n;
+        scanf("%d",&n);
+        insert(n);
+    }
+    bubleSort();
+    print();
+}
